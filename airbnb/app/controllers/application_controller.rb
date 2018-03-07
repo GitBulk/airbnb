79178ec842653b0e13372a7ffdd6d47269ce05af
gitbulk <giacatiensinh@hotmail.com>
2018-03-07 18:08:53 +0700

    [b] fix bug devise permited params

--- airbnb/app/controllers/application_controller.rb
+++ airbnb/app/controllers/application_controller.rb
@@ -3,12 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_action :configure_permitted_params, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def configure_permitted_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :full_name
+    devise_parameter_sanitizer.for(:account_update) << :full_name
+
+    # for devise 4.1+
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
   end
 end
