cb217bdbd6400d1195ea78218f12381441ce1e4d
gitbulk <giacatiensinh@hotmail.com>
2018-03-07 17:24:07 +0700

    [f] 2.4 Authentication with full name

--- airbnb/app/controllers/application_controller.rb
+++ airbnb/app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action: configure_permitted_params, if: :devise_controller?
+
+  private
+
+  def configure_permitted_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:full_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:full_name])
+  end
 end
